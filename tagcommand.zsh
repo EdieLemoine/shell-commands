9478ca123754e8ecc3f84bd63069fefd722296e5
Edie Lemoine <edie@myparcel.nl>
2019-07-30 10:40:05 +0200

    clean up

--- tagcommand.zsh
+++ tagcommand.zsh
@@ -5,43 +5,46 @@ tag() {
   unset currentTag
   unset tag
 
+  # fetch and force update tags
+  git fetch --tags -p -f
+  [[ $? -eq 0 ]] || return # return if git command failed; cwd is likely not a git repository
+
   merges=$(git log --merges --first-parent master --oneline --decorate --stat --pretty="format:%h%<(18)%d %<(20)%cn %<(30)%s")
-  [[ $? -eq 0 ]] || return # return if git command failed
   line=$(echo "$merges" | fzf)
   [[ "$line" ]] || return # return if user canceled command
 
-  commitHash=$(echo ${line} | sed -E 's/^([a-z0-9]+).*$/\1/')
+  commitHash=$(echo "${line}" | sed -E 's/^([a-z\d]+).*$/\1/')
 
   # Get the previous line
-  prevLine=$(echo ${merges} | grep -A 1 "^$commitHash" | tail -1)
+  prevLine=$(echo "${merges}" | grep -A 1 "^$commitHash" | tail -1)
 
   # find current tag if it exists
-  if echo ${line} | grep -q "(tag:"; then
-    currentTag=$(echo ${line} | sed -E 's/^.*([0-9]+\.[0-9]+\.[0-9]+).*$/\1/')
+  if echo "${line}" | grep -q "(tag:"; then
+    currentTag=$(echo "${line}" | sed -E 's/^.*(\d+\.\d+\.\d+).*$/\1/')
   fi
 
-  if echo ${prevLine} | grep -q "(tag:"; then
-    prevTag=$(echo ${prevLine} | sed -E 's/^.*([0-9]+\.[0-9]+\.[0-9]+).*$/\1/')
+  if echo "${prevLine}" | grep -q "(tag:"; then
+    prevTag=$(echo "${prevLine}" | sed -E 's/^.*(\d+\.\d+\.\d+).*$/\1/')
   fi
 
   # Find branch name regardless if commit is merge or pull request
-  if echo ${line} | grep -q 'Merge branch '; then
-    branch=$(echo ${line} | sed -E "s/^.* Merge branch '(.+)'$/\1/")
-  elif echo ${line} | grep -q 'Automatic merge '; then
-    branch=$(echo ${line} | sed -E "s/^.* Automatic merge from (.+) ->.*$/\1/")
-    to=$(echo ${line} | sed -E "s/^.* Automatic merge from .+ -> (.+)$/\1/")
+  if echo "${line}" | grep -q 'Merge branch '; then
+    branch=$(echo "${line}" | sed -E "s/^.* Merge branch '(.+)'$/\1/")
+  elif echo "${line}" | grep -q 'Automatic merge '; then
+    branch=$(echo "${line}" | sed -E "s/^.* Automatic merge from (.+) ->.*$/\1/")
+    to=$(echo "${line}" | sed -E "s/^.* Automatic merge from .+ -> (.+)$/\1/")
   else
-    branch=$(echo ${line} | sed -E "s/^.* Merge pull request.+from (.+) to.*$/\1/")
-    to=$(echo ${line} | sed -E "s/^.* Merge pull request.+from .+ to (.+).*$/\1/")
+    branch=$(echo "${line}" | sed -E "s/^.* Merge pull request.+from (.+) to.*$/\1/")
+    to=$(echo "${line}" | sed -E "s/^.* Merge pull request.+from .+ to (.+).*$/\1/")
   fi
 
   suggestedVersion="?"
-  bumpedPatch=$(semver -i patch ${prevTag})
-  bumpedMinor=$(semver -i minor ${prevTag})
+  bumpedPatch=$(semver -i patch "${prevTag}")
+  bumpedMinor=$(semver -i minor "${prevTag}")
 
   if [[ ${branch} == hotfix/* || ${branch} == bugfix/* ]];  then
     suggestedVersion=$bumpedPatch
-  elif [[ ${branch} == release || ${branch} == release/* || ${branch} == feature/* || ! -z ${prevTag} ]]; then
+  elif [[ ${branch} == release || ${branch} == release/* || ${branch} == feature/* || -n ${prevTag} ]]; then
     suggestedVersion=$bumpedMinor
   fi
 
@@ -81,24 +84,24 @@ tag() {
     tagString=$(echo ${tags} | tr '\n' ', ' | sed 's/,$//')
     echo "\e[93mWarning:\e[39m Commit has tags: \e[93m$tagString\e[39m"
 
-    tagArray=("${(@f)$(echo $tags)}")
+    tagArray=("${(@f)$tags}")
     for commitTag in "${tagArray[@]}"; do
       read -r "deleteTag?Delete $commitTag? (y) "
         if [[ -z ${deleteTag} || ${deleteTag} =~ ^[Yy]$ ]]; then
-           git tag -d ${commitTag} && git push origin --delete ${commitTag}
+           git tag -d "${commitTag}" && git push origin --delete "${commitTag}"
         fi
         unset deleteTag
     done
   fi
 
   if git tag | grep ${tag}; then
-     read "deleteTag?Tag $tag already exits. Delete it first? (y) "
+     read -r "deleteTag?Tag $tag already exits. Delete it first? (y) "
     if [[ -z ${deleteTag} || ${deleteTag} =~ ^[Yy]$ ]]; then
-      git tag -d ${tag} && git push origin --delete ${tag}
+      git tag -d "${tag}" && git push origin --delete "${tag}"
     fi
   fi
 
-  git tag ${tag} "${commitHash}" && git push --tags
+  git tag "${tag}" "${commitHash}" && git push --tags
 
   unset prevTag
   unset currentTag
